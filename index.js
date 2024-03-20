12de7096d1c1d0ff773fb34343879d7d9b5fe60c
Evgeni Anisoev <trash_jack@mail.ru>
2024-03-20 14:57:41 +0300

    add: file/folder add/delete functional

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const os = require('os');
-let res = os.platform();
-console.log(res);
+const fs = require('fs'); //plug file module
 
-const my_math = require('./my_math');
-let res1 = my_math.add(3, 4);
-let res2 = my_math.minus(3, 4);
-console.log(`res1: ${res1}`);
-console.log(`res2: ${res2}`);
+fs.readFile('text.txt', 'utf-8', (err, data) => {
+  fs.writeFile('text.txt', data + '\nNew text', (err, data) => {
+    console.log('Done!');
+  });
+});
+
+fs.mkdir('text-files', () => {
+  console.log('Folder created');
+})
+
+fs.rmdir('./text-files', () => {
+  console.log('Folder deleted');
+})
