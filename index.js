dba214c3a53b43128881fa163d50332e9b147032
Evgeni Anisoev <trash_jack@mail.ru>
2024-03-20 12:33:32 +0300

    add: own module, wok with them

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-let name = 'Evgeni';
-console.log(`My name is ${name}`);
+const os = require('os');
+let res = os.platform();
+console.log(res);
+
+const my_math = require('./my_math');
+let res1 = my_math.add(3, 4);
+let res2 = my_math.minus(3, 4);
+console.log(`res1: ${res1}`);
+console.log(`res2: ${res2}`);
